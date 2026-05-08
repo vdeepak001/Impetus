c8ce42ad5a829492fef7d44b0b70c8342a98390c
vdeepak001 <dv63829@gmail.com>
2026-05-08 10:32:04 +0530

    update view progress

--- resources/views/practice-test.blade.php
+++ resources/views/practice-test.blade.php
@@ -141,7 +141,7 @@
                                                                 x-transition:enter="transition ease-out duration-200"
                                                                 x-transition:enter-start="opacity-0 translate-y-1"
                                                                 x-transition:enter-end="opacity-100 translate-y-0"
-                                                                class="absolute top-full left-1/2 z-[60] mt-3 w-56 -translate-x-1/2"
+                                                                class="absolute left-full top-1/2 z-[60] ml-3 w-64 -translate-y-1/2"
                                                                 style="display: none;"
                                                             >
                                                                 <div class="relative overflow-hidden rounded-xl border border-slate-200 bg-white p-3 shadow-2xl ring-1 ring-black/5">
@@ -166,7 +166,7 @@
                                                                     </div>
                                                                 </div>
                                                                 {{-- Arrow --}}
-                                                                <div class="absolute -top-1.5 left-1/2 h-3 w-3 -translate-x-1/2 rotate-45 border-l border-t border-slate-200 bg-white"></div>
+                                                                <div class="absolute -left-1.5 top-1/2 h-3 w-3 -translate-y-1/2 rotate-45 border-b border-l border-slate-200 bg-white"></div>
                                                             </div>
                                                         </div>
                                                     </td>
