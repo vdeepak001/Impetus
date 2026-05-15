67cc37110a9cbdf1faea5a0c5ad4122caf3b46e4
vdeepak001 <dv63829@gmail.com>
2026-05-15 14:45:00 +0530

    upate pretest

--- resources/views/super-admin/order-status/index.blade.php
+++ resources/views/super-admin/order-status/index.blade.php
@@ -71,7 +71,7 @@
                             <td class="px-6 py-4 text-sm font-medium text-gray-600 dark:text-gray-400">{{ $order->courseDetail->couse_name ?? 'N/A' }}</td>
                             <td class="px-6 py-4 text-sm font-medium text-gray-600 dark:text-gray-400">{{ $order->created_at->format('d-m-Y') }}</td>
                             <td class="px-6 py-4 text-sm font-medium text-gray-600 dark:text-gray-400">{{ $order->created_at->format('h:i A') }}</td>
-                            <td class="px-6 py-4 text-sm font-bold text-center">
+                            <td class="px-6 py-4 text-sm font-normal text-center">
                                 @php
                                     $statusText = match($order->payment_status->value) {
                                         'completed' => 'Success',
