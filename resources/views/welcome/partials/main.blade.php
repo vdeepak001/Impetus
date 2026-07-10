3fd7848710de1ade144bcdb33381bd58e124705d
vdeepak001 <dv63829@gmail.com>
2026-07-10 12:44:55 +0530

    update code

--- resources/views/welcome/partials/main.blade.php
+++ resources/views/welcome/partials/main.blade.php
@@ -8,7 +8,7 @@
             <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-logo-blue/20 to-logo-light-green/30 opacity-40 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
         </div>
 
-        <div class="max-w-7xl mx-auto px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
+        <div class="max-w-7xl mx-auto px-6 py-8 sm:py-10 lg:px-8 lg:py-12">
             <div class="lg:grid lg:grid-cols-12 lg:gap-16 items-center">
                 <div class="lg:col-span-6 text-center lg:text-left">
 
