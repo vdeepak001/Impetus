be5a53c67f8dde29464e419cd3233505b32dabfd
vdeepak001 <dv63829@gmail.com>
2026-09-15 15:34:42 +0530

    update

--- resources/views/livewire/super-admin/users-list/index.blade.php
+++ resources/views/livewire/super-admin/users-list/index.blade.php
@@ -123,11 +123,11 @@
                         </th>
                         <th scope="col"
                             class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300">
-                            Unique ID
+                            UID
                         </th>
                         <th scope="col"
                             class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300">
-                            Sequence Number
+                            VLSID
                         </th>
                         <th scope="col"
                             class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300">
